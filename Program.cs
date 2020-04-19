52d43bc319f58706e2aa334caf0c9398d864c142
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-18 23:33:18 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring variables
+            string part1 = "Hello";
+            string part2 = "Word";
+            string space = " ";
+            object textPrint = "";
+
+            //concatenating strings
+            textPrint = part1 + space + part2;
+
+            //showing the message
+            Console.WriteLine("{0}", textPrint);
+
+            Console.ReadKey();
+                
+        }
+    }
+}
